638400eb4c41d837c0667bf8ffa6403ce88c657a
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2022-10-15 15:44:37 +0800

    feat: start uart module

---
+++ src/uart.c
@@ -0,0 +1,9 @@
+#include "uart.h"
+
+/*
+ * @brief Initializes the uart module.
+ */
+void uart_init(void)
+{
+
+}
---
+++ test/test_uart.c
@@ -0,0 +1,18 @@
+#ifdef TEST
+
+#include "unity.h"
+
+#define EXTERN 
+
+#include "testable_mcu_registers.h"
+#include "uart.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+#endif // TEST
