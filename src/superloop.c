d122d77aacf323237b03986cb0f75cce5d57ebf5
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2023-02-26 14:52:58 +0800

    feat: move docs to trigger on main push

--- src/superloop.c
+++ src/superloop.c
@@ -15,7 +15,7 @@
 #include "testable_mcu_registers.h"
 #endif
 
-#define ONE_SEC_IN_MS      (1000U)
+#define ONE_SEC_IN_MS (1000U)
 
 STATIC_ASSERT(DISPLAY_WIDTH == FRAMEBUFFER_WIDTH, display_and_framebuffer_widths_do_not_match);
 STATIC_ASSERT(DISPLAY_HEIGHT == FRAMEBUFFER_HEIGHT, display_and_framebuffer_heights_do_not_match);
@@ -35,7 +35,6 @@ extern void superloop_init(void)
     framebuffer_init();
 
     uart_init();
-
     MY_PRINTF("superloop_init() complete.\r\n");
 }
 
