bf3d370d021aeb9c62c071bfef522760a7d67ff4
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2026-01-26 16:42:27 +0000

    some basic idea

--- inc/drivers/board.h
+++ inc/drivers/board.h
@@ -1,7 +1,10 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#include <stdint.h>
+
 void set_led_pin_as_output(void);
+void set_pin_as_output(uint8_t port, uint8_t pin);
 void turn_led_pin_off(void);
 void turn_led_pin_on(void);
 void toggle_led_pin(void);
--- src/drivers/board.c
+++ src/drivers/board.c
@@ -1,31 +1,56 @@
 #include "drivers/board.h"
 
+#include "utils/my_assert.h"
+
 #ifndef TEST
 #include "aduc706x.h"
 #else
 #include "testable_mcu_registers.h"
 #endif
 
-void set_led_pin_as_output(void)
+#define LED_PORT (1U)
+#define LED_PIN  (5U)
+
+void set_led_pin_as_output()
 {
-    /* configure P1.5 as an output */
-    GP1DAT |= (1UL << 29);
+    set_pin_as_output(LED_PORT, LED_PIN);
+}
+
+void set_pin_as_output(uint8_t port, uint8_t pin)
+{
+    MY_ASSERT(port <= 2);
+    MY_ASSERT(pin <= 7);
+
+    uint32_t pin_to_set = (1UL << (24 + pin));
+
+    if(port == 0)
+    {
+        GP0DAT |= pin_to_set;
+    }
+    else if(port == 1)
+    {
+        GP1DAT |= pin_to_set;
+    }
+    else
+    {
+        GP2DAT |= pin_to_set;
+    }
 }
 
 void turn_led_pin_off(void)
 {
     /* turn P1.5 off */
-    GP1DAT &= ~(1UL << 21);
+    GP1DAT &= ~(1UL << (16 + 5));
 }
 
 void turn_led_pin_on(void)
 {
     /* turn P1.5 on */
-    GP1DAT |= (1UL << 21);
+    GP1DAT |= (1UL << (16 + 5));
 }
 
 void toggle_led_pin(void)
 {
     /* toggle P1.5 */
-    GP1DAT ^= (1UL << 21);
+    GP1DAT ^= (1UL << (16 + 5));
 }
