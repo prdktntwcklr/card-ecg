87e442ac4e54a6f975ace2d67563bb62b9512450
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2026-01-31 06:11:40 +0000

    revert to non-generic driver

--- inc/drivers/board.h
+++ inc/drivers/board.h
@@ -4,7 +4,6 @@
 #include <stdint.h>
 
 void set_led_pin_as_output(void);
-void set_pin_as_output(uint8_t port, uint8_t pin);
 void turn_led_pin_off(void);
 void turn_led_pin_on(void);
 void toggle_led_pin(void);
--- src/drivers/board.c
+++ src/drivers/board.c
@@ -1,56 +1,35 @@
 #include "drivers/board.h"
 
-#include "utils/my_assert.h"
-
 #ifndef TEST
 #include "aduc706x.h"
 #else
 #include "testable_mcu_registers.h"
 #endif
 
-#define LED_PORT (1U)
-#define LED_PIN  (5U)
+// TODO: make this driver more generic
+
+#define LED_PIN (5)
 
 void set_led_pin_as_output()
 {
-    set_pin_as_output(LED_PORT, LED_PIN);
-}
-
-void set_pin_as_output(uint8_t port, uint8_t pin)
-{
-    MY_ASSERT(port <= 2);
-    MY_ASSERT(pin <= 7);
-
-    uint32_t pin_to_set = (1UL << (24 + pin));
-
-    if(port == 0)
-    {
-        GP0DAT |= pin_to_set;
-    }
-    else if(port == 1)
-    {
-        GP1DAT |= pin_to_set;
-    }
-    else
-    {
-        GP2DAT |= pin_to_set;
-    }
+    /* set P1.5 as output */
+    GP1DAT |= (1UL << (24 + LED_PIN));
 }
 
 void turn_led_pin_off(void)
 {
     /* turn P1.5 off */
-    GP1DAT &= ~(1UL << (16 + 5));
+    GP1DAT &= ~(1UL << (16 + LED_PIN));
 }
 
 void turn_led_pin_on(void)
 {
     /* turn P1.5 on */
-    GP1DAT |= (1UL << (16 + 5));
+    GP1DAT |= (1UL << (16 + LED_PIN));
 }
 
 void toggle_led_pin(void)
 {
     /* toggle P1.5 */
-    GP1DAT ^= (1UL << (16 + 5));
+    GP1DAT ^= (1UL << (16 + LED_PIN));
 }
