6dbd3f7c8d4a1f52f72bbff139104df027506c6c
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2026-01-31 06:32:19 +0000

    abstract a bit more

--- src/drivers/board.c
+++ src/drivers/board.c
@@ -7,29 +7,35 @@
 #endif
 
 // TODO: make this driver more generic
+#define LED_PORT_NO (1UL) // currently unused
+#define LED_PIN_NO  (5UL)
 
-#define LED_PIN_NO (5UL)
+// see p103 of datasheet
+#define GPIO_DATA_BIT_OFFSET      (16)
+#define GPIO_DIR_BIT_OFFSET       (24)
+
+#define GPIO_BIT_MASK(shift, pin) (1UL << (uint8_t)((shift) + (pin)))
 
 void set_led_pin_as_output()
 {
     /* set P1.5 as output */
-    GP1DAT |= (1UL << (uint8_t)(24 + LED_PIN_NO));
+    GP1DAT |= GPIO_BIT_MASK(GPIO_DIR_BIT_OFFSET, LED_PIN_NO);
 }
 
 void turn_led_pin_off(void)
 {
     /* turn P1.5 off */
-    GP1DAT &= ~(1UL << (uint8_t)(16 + LED_PIN_NO));
+    GP1DAT &= ~GPIO_BIT_MASK(GPIO_DATA_BIT_OFFSET, LED_PIN_NO);
 }
 
 void turn_led_pin_on(void)
 {
     /* turn P1.5 on */
-    GP1DAT |= (1UL << (uint8_t)(16 + LED_PIN_NO));
+    GP1DAT |= GPIO_BIT_MASK(GPIO_DATA_BIT_OFFSET, LED_PIN_NO);
 }
 
 void toggle_led_pin(void)
 {
     /* toggle P1.5 */
-    GP1DAT ^= (1UL << (uint8_t)(16 + LED_PIN_NO));
+    GP1DAT ^= GPIO_BIT_MASK(GPIO_DATA_BIT_OFFSET, LED_PIN_NO);
 }
